803a2bd75a91c9cb2e39bee86326fb3976c3cb93
xwinkX <opalyuk.e88@ukr.net>
2022-06-21 18:57:43 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
-const button = document.querySelector('button');
-const input1 = document.getElementById('num1')! as HTMLInputElement;
-const input2 = document.getElementById('num2')! as HTMLInputElement;
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
 
 function add(num1: number, num2: number) {
     return num1 + num2;
